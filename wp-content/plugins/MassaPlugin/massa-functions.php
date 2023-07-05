e15518fdf8d9909d18e26b76434b5cf0eed56c76
Meital <arararmm@gmail.xom>
2023-07-05 21:37:45 +0300

    Xammp Project

--- wp-content/plugins/MassaPlugin/massa-functions.php
+++ wp-content/plugins/MassaPlugin/massa-functions.php
@@ -161,6 +161,6 @@ function display_users() {
 // Function to display text
 function display_text() {
   echo '<h2>Just Another Text</h2>';
-  echo '<p>BaliApp</p>';
+  echo '<p>Baliii App</p>';
 }
 
